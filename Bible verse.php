0535d7a47b6b96b6f60c45ca0391e874c56fc6d0
itsjomar <73252539+jmrbntz29@users.noreply.github.com>
2021-03-12 16:12:17 +0800

    Create Bible verse.php

---
+++ Bible verse.php
@@ -0,0 +1,125 @@
+
+    
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset=”utf-8”>
+        <title></title>
+        <link rel=”stylesheet” href=”../styles.css”>
+        <script src=https://kit.fontawesome.com/yourcode.js></script>
+    </head>
+    <body>
+
+        <nav>
+           <label class=”logo”>Online Bible</label>
+            <ul>
+            <li><a class=”active” href=”../index.html”>Home</a></li>
+             <li><a href=”#”>Book</a></li>
+             <li><a href=”../index.html”>Chapter</a></li>
+             <li><a href=”#”>Verse</a></li>
+           </ul>
+        </nav>
+
+        <div class=”wrapper”>
+                <h1>Amos <br>1</h1>
+            <div class=”content”>
+<strong>1</strong>The words of Amos, one of the shepherds of Tekoa(A)—the vision he saw concerning Israel two years before the earthquake,(B) when Uzziah(C) was king of Judah and Jeroboam(D) son of Jehoash[a] was king of Israel.(E)
+ <strong>2</strong>He said:
+
+“The Lord roars(F) from Zion
+    And thunders(G) from Jerusalem;(H)
+The pastures of the shepherds dry up,
+    And the top of Carmel(I) withers.”(J)
+ <strong>3</strong> This is what the Lord says:
+
+“For three sins of Damascus,(K)
+    Even for four, I will not relent.(L)
+Because she threshed Gilead
+    With sledges having iron teeth,
+<strong>4</strong> I will send fire(M) on the house of Hazael(N)
+    That will consume the fortresses(O) of Ben-Hadad.(P)
+<strong>5</strong>I will break down the gate(Q) of Damascus;
+    I will destroy the king who is in[b] the Valley of Aven[c]
+And the one who holds the scepter in Beth Eden.(R)
+    The people of Aram will go into exile to Kir,(S)”
+Says the Lord.(T)
+<strong>6</strong> This is what the Lord says:
+
+“For three sins of Gaza,(U)
+    Even for four, I will not relent.(V)
+Because she took captive whole communities
+    And sold them to Edom,(W)
+<strong>7</strong>I will send fire on the walls of Gaza
+    That will consume her fortresses.
+<strong>8</strong>I will destroy the king[d] of Ashdod(X)
+    And the one who holds the scepter in Ashkelon.
+I will turn my hand(Y) against Ekron,
+    Till the last of the Philistines(Z) are dead,”(AA)
+Says the Sovereign Lord.(AB)
+ <strong>9</strong>This is what the Lord says:
+
+“For three sins of Tyre,(AC)
+    Even for four, I will not relent.(AD)
+Because she sold whole communities of captives to Edom,
+    Disregarding a treaty of brotherhood,(AE)
+<strong>10</strong>I will send fire on the walls of Tyre
+    That will consume her fortresses.(AF)”
+  <strong>11</strong> This is what the Lord says:
+
+“For three sins of Edom,(AG)
+    Even for four, I will not relent.
+Because he pursued his brother with a sword(AH)
+    And slaughtered the women of the land,
+Because his anger raged continually
+    And his fury flamed unchecked,(AI
+<strong>12</strong>I will send fire on Teman(AJ)
+    That will consume the fortresses of Bozrah.(AK)”
+<strong>13</strong> This is what the Lord says:
+
+“For three sins of Ammon,(AL)
+    Even for four, I will not relent.
+Because he ripped open the pregnant women(AM) of Gilead
+    In order to extend his borders,
+<strong>14</strong> I will set fire to the walls of Rabbah(AN)
+    That will consume(AO) her fortresses
+Amid war cries(AP) on the day of battle,
+    Amid violent winds(AQ) on a stormy day.
+<strong>15</strong>Her king[e] will go into exile,
+    He and his officials together,(AR)”
+Says the Lord.(AS)
+
+
+
+</div>
+        </div>
+        <a href=”../index.html” class=”previous”>&laquo; Previous</a>
+        <a href=”../PAGE /chapter 1.html” class=”next”>Next &raquo;</a> <br><br><br>
+   </body> 
+   <footer class=”footer-main-div”> <hr> 
+    <h1>Online Bible</h1><hr>
+    <h3>About Us</h3>
+    <p> Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+        <br>Lorem Ipsum has been the industry’s standard dummy text ever since the 
+        <br>1500s,when an unknown printer took a galley of type and scrambled it to 
+         <br>make a type specimen book.It has survived not only five centuries, but 
+         <br>also the leap into electronic typesetting, remaining essentially unchanged. 
+         <br>It was popularised in the 1960s with the release of Letraset sheets containing
+         <br> Lorem Ipsum passages, and more recently with desktop publishing software like 
+         <br>Aldus PageMaker including versions of Lorem Ipsum.</p>    
+     <p> Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+            <br>Lorem Ipsum has been the industry’s standard dummy text ever since the 
+            <br>1500s,when an unknown printer took a galley of type and scrambled it to 
+             <br>make a type specimen book.It has survived not only five centuries, but 
+             <br>also the leap into electronic typesetting, remaining essentially unchanged. 
+             <br>It was popularised in the 1960s with the release of Letraset sheets containing
+             <br> Lorem Ipsum passages, and more recently with desktop publishing software like 
+             <br>Aldus PageMaker including versions of Lorem Ipsum.</p>                    
+    <h4>Social Media:</h4>
+    <hr>
+    <h5>
+        Copyright Online Bible |
+
+        Privacy Policy |
+
+        Term Of Use
+    </h5>  
